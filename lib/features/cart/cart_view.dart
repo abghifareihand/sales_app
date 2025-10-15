2ea30201a6240eac962366d1d0d1e6626c6ef5b8
abghifareihand <info.abghifareihan@gmail.com>
2025-10-16 06:41:54 +0700

    update cart detail

--- lib/features/cart/cart_view.dart
+++ lib/features/cart/cart_view.dart
@@ -59,13 +59,53 @@ Widget _buildBody(BuildContext context) {
                   Text(
                     product.name ?? '',
                     style: AppFonts.semiBold.copyWith(color: AppColors.black, fontSize: 16),
+                    overflow: TextOverflow.ellipsis,
                   ),
+                  Text(
+                    product.provider ?? '-',
+                    style: AppFonts.medium.copyWith(
+                      color: AppColors.black,
+                      fontSize: 12,
+                      height: 1.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    product.category ?? '-',
+                    style: AppFonts.medium.copyWith(color: AppColors.black, fontSize: 12),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    product.kuota ?? '-',
+                    style: AppFonts.medium.copyWith(
+                      color: AppColors.black,
+                      fontSize: 10,
+                      height: 1.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        Formatter.toRupiahDouble(product.sellingPrice ?? 0),
-                        style: AppFonts.medium.copyWith(color: AppColors.black, fontSize: 14),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            Formatter.toRupiahDouble(product.sellingPrice ?? 0),
+                            style: AppFonts.medium.copyWith(color: AppColors.black, fontSize: 14),
+                          ),
+                          Text(
+                            'Sisa stok: ${product.quantity ?? 0}',
+                            style: AppFonts.regular.copyWith(
+                              color: AppColors.black.withValues(alpha: 0.5),
+                              fontSize: 10,
+                              height: 1.0,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
                       ),
                       Row(
                         children: [
